c8c67414a4b3bad57c9fc154f19235bee1b7e047
Park-Wonbin <wonbin9413@postech.ac.kr>
2018-04-21 16:48:39 +0900

    main view

---
+++ inpostack/FacilityViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FacilityViewController.swift
+//  inpostack
+//
+//  Created by 박원빈 on 2018. 4. 21..
+//  Copyright © 2018년 inpostack-ios. All rights reserved.
+//
+
+import UIKit
+
+class FacilityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
